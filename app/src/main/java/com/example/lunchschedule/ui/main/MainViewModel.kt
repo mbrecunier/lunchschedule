c2b080746fc5e226bdf97dddb395f6c9ae5e1710
Michelle Brecunier <mbrecunier@mbrec.local>
2021-07-30 14:03:15 -0700

    create Menu data class for storing multiple courses in a menu

--- app/src/main/java/com/example/lunchschedule/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/lunchschedule/ui/main/MainViewModel.kt
@@ -22,16 +22,35 @@ class MainViewModel : ViewModel() {
         // Generate menu mapping for 1/1/21 - 12/31/21
         var date = LocalDate.of(2021, 1, 1)
         val endDate = LocalDate.of(2021, 12, 31)
-        val menu: List<String> = listOf("Chicken and waffles", "Tacos", "Curry", "Pizza", "Sushi",
-            "No Catering", "No Catering", "Breakfast for lunch", "Hamburgers", "Spaghetti",
-            "Salmon", "Sandwiches", "No Catering", "No Catering")
+        val menu: List<Menu> = listOf(
+            Menu("Cheese Sticks", "Chicken and waffles", "Ice Cream"),
+            Menu("Cheese Sticks", "Tacos", "Ice Cream"),
+            Menu("Cheese Sticks", "Curry", "Ice Cream"),
+            Menu("Cheese Sticks", "Pizza", "Ice Cream"),
+            Menu("Cheese Sticks", "Sushi", "Ice Cream"),
+            Menu("Cheese Sticks", "No Catering", "Ice Cream"),
+            Menu("Cheese Sticks", "No Catering", "Ice Cream"),
+            Menu("Cheese Sticks", "Breakfast for lunch", "Ice Cream"),
+            Menu("Cheese Sticks", "Hamburgers", "Ice Cream"),
+            Menu("Cheese Sticks", "Spaghetti", "Ice Cream"),
+            Menu("Cheese Sticks", "Salmon", "Ice Cream"),
+            Menu("Cheese Sticks", "Sandwiches", "Ice Cream"),
+            Menu("Cheese Sticks", "No Catering", "Ice Cream"),
+            Menu("Cheese Sticks", "No Catering", "Ice Cream")
+        )
         var mealIndex = 4
 
         while (!date.isAfter(endDate)) {
-            lunchMenuMap[date] = menu[mealIndex]
+            lunchMenuMap[date] = menu[mealIndex].displayMenuString()
             date = date.plusDays(1)
             mealIndex++
             if (mealIndex >= menu.size) mealIndex = 0
+        }
+    }
+
+    data class Menu(val appetizer: String? = "Cheese Sticks", val entree: String?, val dessert: String? = "Ice Cream") {
+        fun displayMenuString(): String {
+            return "Appetizer: $appetizer  Entree: $entree  Dessert: $dessert"
         }
     }
 }
